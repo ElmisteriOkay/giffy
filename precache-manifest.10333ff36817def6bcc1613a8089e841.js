5507cdb90aa6283fddabb5297f7979475c4e81fb
ElmisteriOkay <walkpeting@gmail.com>
2021-07-24 13:50:21 -0500

    Updates

---
+++ precache-manifest.10333ff36817def6bcc1613a8089e841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd2b0e3dc361fb9bf89d4fa4e4d69f1",
+    "url": "/ElmisteriOkay.github.io/giffy/index.html"
+  },
+  {
+    "revision": "6db2875b1b4f987fca35",
+    "url": "/ElmisteriOkay.github.io/giffy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7b26caa45911b5de52eb",
+    "url": "/ElmisteriOkay.github.io/giffy/static/js/2.c320036a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ElmisteriOkay.github.io/giffy/static/js/2.c320036a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db2875b1b4f987fca35",
+    "url": "/ElmisteriOkay.github.io/giffy/static/js/main.8666cfd8.chunk.js"
+  },
+  {
+    "revision": "a25f0bbcd8f17dc31f6d",
+    "url": "/ElmisteriOkay.github.io/giffy/static/js/runtime-main.398a7eee.js"
+  }
+]);
